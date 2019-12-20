9bd28d8079e67ac1f11ded1d43a2e3829131c34c
Silas Salema <silazdasilva20@gmail.com>
2019-12-20 10:33:09 -0300

    Lista de Funcionários

---
+++ Exercicio_Lista/Funcionario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Exercicio_Lista
+{
+    class Funcionario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Salario { get; set; }
+
+        public void IncrementaSalario(double s)
+        {
+            Salario = Salario + Salario * (s/100);
+        }
+        public override string ToString()
+        {
+            return Id + ", "+ Nome + ", " + Salario.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Exercicio_Lista/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Exercicio_Lista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Funcionario> list = new List<Funcionario>();
+            Console.Write("Quantos funcionários serão registrados?:");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("Funcionário: #" + i);
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Salario: ");
+                double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                list.Add(new Funcionario { Id = id, Nome = nome, Salario = salario });
+            }
+
+            foreach (Funcionario obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+
+            Console.Write("Informe o Id do funcionário que receberá o aumento: ");
+            int pesquisaId = int.Parse(Console.ReadLine());
+
+            Funcionario func = list.Find(x => x.Id == pesquisaId);
+            
+            if (func == null)
+            {
+                Console.WriteLine("Este id não existe");
+            }
+            else
+            {
+                Console.Write("Entre com a porcetagem do aumento:");
+                double aumento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                func.IncrementaSalario(aumento);
+            }
+
+            Console.WriteLine("Atualização da lista de funcionários: ");
+            foreach(Funcionario obj in list) {
+                Console.WriteLine(obj);
+            }
+            
+            
+        }
+    }
+}
